65dbde6a3e8ff5520728af844bff8f29080e3918
Carlos Josué Méndez Núñez <carlosmendeznunez07@gmail.com>
2025-09-06 23:12:25 -0600

    Archivo de comando vacio

---
+++ app/Console/Commands/ProcesarPendientesParcelsApp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ProcesarPendientesParcelsApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:procesar-pendientes-parcels-app';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
