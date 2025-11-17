031f291978536d633ea0aa1149a58e44507530ae
ldrichmondg <luis.richmondg@gmail.com>
2025-11-17 12:16:48 -0600

    -Cambiar una variable para que funcione

--- app/Services/Proveedores/Aeropost/ServicioAeropost.php
+++ app/Services/Proveedores/Aeropost/ServicioAeropost.php
@@ -218,7 +218,7 @@ class ServicioAeropost implements InterfazProveedor
 
         // 3. Sincronizar los encabezados de los trackings
         // 3.1. Obtener todos los paquetes que hay en Aeropost
-        $trackingsAeropost = $this->apiClient->ObtenerPaquetesMasivos($numerosTracking, true);
+        $trackingsAeropost = $this->apiClient->ObtenerPaquetesMasivos($numerosTracking, false);
         Log::info('[SA, SET] CANTIDAD TODOS: ' . count($trackingsAeropost));
 
         DB::transaction(function () use ($trackings, &$trackingsAeropost) {
